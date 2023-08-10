3bffd3d3aadedfdb6b0f804d66c3ed84137ded00
DeltaStas365 <mistergast6@gmail.com>
2023-08-10 18:23:52 +0300

    First Site Models darn it

--- first_site/games/models.py
+++ first_site/games/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Profile(models.Model):
+    avatar = models.ImageField(upload_to='avatar', blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Category(models.Model):
+    name = models.CharField(max_length=16)
+
+class Game(models.Model):
+    name = models.CharField(max_length=16, unique=True)
+    desc = models.TextField(blank=True, null=True)
+    min_price = models.IntegerField()
+    max_price = models.IntegerField()
+    preview_image = models.ImageField(upload_to='games_previews', blank=True, null=True)
+    reviews_count = models.IntegerField(default=0)
+    rate = models.DecimalField(default=0, max_digits = 2, decimal_places=1)
+    categories = models.ManyToManyField(Category)
+
+class Review(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    rate = models.IntegerField()
+    text = models.TextField()
+class Complilation(models.Model):
+    game = models.ManyToManyField(Game)
+    name = models.CharField(max_length=32)
+    desc_comp = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='comp_img', blank=True, null=True)
