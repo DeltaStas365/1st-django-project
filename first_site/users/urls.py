36fba55f1df014cc42388018dd2609067e8dd731
DeltaStas365 <mistergast6@gmail.com>
2024-01-28 18:19:45 +0300

    Almost last update

--- first_site/users/urls.py
+++ first_site/users/urls.py
@@ -1,7 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from .views import *
 urlpatterns = [
     path("profile/", profile, name='profile'),
     path("register/", registration, name="signup"),
     path("profile/edit", profile_edit, name="edit_profile"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
